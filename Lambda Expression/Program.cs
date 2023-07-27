ec12bed102b9f1d8b1f57dfa8a08fc59128c5146
mkhoyanarmen <93216230+mkhoyanarmen@users.noreply.github.com>
2023-07-27 12:30:36 +0400

    Add project files.

---
+++ Lambda Expression/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Expression
+{
+    internal class Program
+    {
+        delegate int SubDel(int a, int b);
+        static void Main(string[] args)
+        {
+            SubDel op = (a, b) => a + b;
+            Console.WriteLine(op(5, 6));
+
+            int sum = new SubDel((a, b) => a - b).Invoke(89, 5);
+            Console.WriteLine(sum);
+
+            Func();
+        }
+        static void Func() => Console.WriteLine("The End");
+    }
+}
